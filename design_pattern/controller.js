4623cbd53f30c76c951571d4c80812462cb56af1
jordan <jojo94320@gmail.com>
2016-06-30 14:56:56 +0000

    Append module express-minify-html

--- design_pattern/controller.js
+++ design_pattern/controller.js
@@ -10,18 +10,9 @@ module.exports = function(app){
 	
 	function f404(req, res)
 	{
-        res.status(404).render('404.twig', {'url' : req.url},function(err, html){
-        	minifyHtml(err, html, res);
-        });
+        res.status(404).render('404', {'url' : req.url});
 	}
 	
-
-	
-	function minifyHtml(err, html, res)
-	{
-		html = html.replace(/\n/g, '').replace(/>\s*</g, '><');
-		res.send(html);
-	}
 	
 	
 	return {
